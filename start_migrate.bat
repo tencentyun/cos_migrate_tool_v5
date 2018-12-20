1f24881a7679b1b97822776787846f231ff9fecc
kitmanzheng <kitmanzheng@gmail.com>
2018-12-20 10:42:21 +0800

    Update start_migrate.bat

--- start_migrate.bat
+++ start_migrate.bat
@@ -1,6 +1,6 @@
 @echo off
 set cur_dir=%CD%
 cd %cur_dir%
-set my_java_cp=.;%cur_dir%\dep\*;%cur_dir%\src\main\resources
+set my_java_cp=%cur_dir%\src\main\resources;.;%cur_dir%\dep\*
 java -Dfile.encoding=UTF-8 -cp "%my_java_cp%" com.qcloud.cos_migrate_tool.app.App
 pause>nul
